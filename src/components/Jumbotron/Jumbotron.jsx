76f65756d86eca19351e43bbd39fe3c5a94fdf09
André Loezer <andrecesarloezer@gmail.com>
2022-09-28 16:02:49 -0300

    Jumbotron section is now a dynamic gallery

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,34 +1,102 @@
 import './Jumbotron.css';
-import background from '../../images/jumbotron.png';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Jumbotron = () => {
+
+    const GALLERY_UPDATE_DELAY = 5000;
+
+    const [ selectedItem, setSelectedItem ] = useState(2);
+
+    const [ gallery, setGallery ] = useState([
+        {
+            background: '/images/concert.png',
+            top: 'Lorem ipsum',
+            mid: 'dolor sit',
+            bottom: 'amet consectetur',
+            selected: true,
+        },
+        {
+            background: '/images/trending.png',
+            top: 'dolor sit',
+            mid: 'Lorem ipsum',
+            bottom: 'amet consectetur',
+            selected: false,
+        },
+        {
+            background: '/images/jumbotron.png',
+            top: 'A Safe Kind',
+            mid: 'Music Is',
+            bottom: 'Of High',
+            selected: false,
+        },
+        {
+            background: '/images/hire-us.png',
+            top: 'amet consectetur',
+            mid: 'dolor sit',
+            bottom: 'Lorem ipsum',
+            selected: false,
+        }
+    ]);
+
+    const changeGallery = (new_index) => { setSelectedItem(new_index) };
+
+    // Alternate between the gallery items
+    useEffect(() => {
+        let index = selectedItem;
+        setInterval(() => {
+            changeGallery(index);
+            index++;
+            if (index === gallery.length) { index = 0; }
+        }, GALLERY_UPDATE_DELAY);
+    }, []);
+
+    const clickHandler = (index) => {
+        changeGallery(index);
+    };
+
+    const selectorList = [];
+    const galleryList = [];
+
+    gallery.forEach((item, index) => {
+        selectorList.push(
+            <li
+                key={index}
+                onClick={() => clickHandler(index)}
+                className={`selector-item${selectedItem === index ? ' selected' : ''}`}
+            >
+                <hr size='1' />
+                <span>{index + 1}</span>
+                <hr size='1' />
+            </li>
+        );
+
+        galleryList.push(
+            <li
+                key={index}
+                className={`gallery-item${selectedItem === index ? ' selected' : ''}`}
+            >
+                <div className='gallery-background'>
+                    <img src={item.background} alt={`background-${index + 1}`} />
+                </div>
+                <div className="text">
+                    <span className='top-text'>{item.top}</span>
+                    <span className='mid-text'>{item.mid}</span>
+                    <span className='bottom-text'>{item.bottom}</span>
+                </div>
+            </li>
+        );
+    });
+
     return (
-        <section
-            className='jumbotron'
-            style={{ backgroundImage: `url(${background})` }}
-        >
+        <section className='jumbotron' >
             <div className='background'></div>
-            <div className='decorations'>
-                <div>
-                    <span>1</span>
-                </div>
-                <div>
-                    <span>2</span>
-                </div>
-                <div className='striked'>
-                    <hr size='1' />
-                    <span>3</span>
-                    <hr size='1' />
-                </div>
-                <div>
-                    <span>4</span>
-                </div>
-            </div>
-            <div className='text'>
-                <span className='top-text'>A Safe Kind</span>
-                <span className='mid-text'>Music Is</span>
-                <span className='bottom-text'>Of High</span>
-            </div>
+            <ul className="selector">
+                {selectorList}
+            </ul>
+            <ul className='gallery'>
+                {galleryList}
+            </ul>
         </section>
     );
 };
