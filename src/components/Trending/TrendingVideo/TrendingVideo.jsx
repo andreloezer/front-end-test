1d5e3e93a6c23be796eba8af2a3e3207c7111219
André Loezer <andrecesarloezer@gmail.com>
2022-09-29 21:30:34 -0300

    Trending section progress bar now is clickable and updates the video progress

--- src/components/Trending/TrendingVideo/TrendingVideo.jsx
+++ src/components/Trending/TrendingVideo/TrendingVideo.jsx
@@ -4,7 +4,7 @@ import './TrendingVideo.css';
 
 const TrendingVideo = (props) => {
 
-    const [ videoProgress, setVideoProgress ] = useState(0);
+    const [ progressBarWidth, setProgressBarWidth ] = useState(0);
 
     const UPDATE_DELAY = 100;
     
@@ -17,9 +17,21 @@ const TrendingVideo = (props) => {
             const duration = videoEl.current.duration;
             const currentProgress = 100 - ((duration - currentTime) / duration * 100);
 
-            setVideoProgress(currentProgress);
+            setProgressBarWidth(currentProgress);
         }, UPDATE_DELAY);
     }, []);
+
+    // Update video progress
+    const progressbarClickHandler = (event) => {
+        const barWidth = event.target.clientWidth;
+        const clickedX = event.clientX;
+
+        const newProgress = 100 - ((barWidth - clickedX) / barWidth * 100);
+        const newVideoTime = newProgress * videoEl.current.duration / 100;
+
+        videoEl.current.currentTime = newVideoTime;
+        setProgressBarWidth(newProgress);
+    };
 
     const progressBarClass = props.showControls ? 'progress-bar hidden' : 'progress-bar';
 
@@ -38,9 +50,13 @@ const TrendingVideo = (props) => {
                 muted={!props.showControls}
             ></video>
             <hr
-                style={{width: `${videoProgress}%`}}
+                style={{width: `${progressBarWidth}%`}}
                 className={progressBarClass}
             ></hr>
+            <hr
+                className="progressBarClickable"
+                onClick={progressbarClickHandler}
+            />
         </Fragment>
     );
 };
